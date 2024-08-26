082985ef345d6fed3033c6a50e5a5ead6f5dceb9
Sicakyuz <cigdem.sicakyuz@gmail.com>
2024-08-26 10:35:39 +0300

    filepath is changed

--- file_upload.py
+++ file_upload.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import streamlit as st
+import requests
+import tempfile
+
 
 def check_date_column(df):
     """Check and convert 'TARIH' column to datetime format."""
@@ -16,6 +19,39 @@ def check_date_column(df):
         except Exception as e:
             st.error(f"Error converting 'TARIH' column to datetime format: {e}")
             return None
+
+
+def download_file_from_google_sheets(file_id):
+    """Download a Google Sheets file as an Excel file and return a file-like object."""
+    url = f'https://docs.google.com/spreadsheets/d/{file_id}/export?format=xlsx'
+    response = requests.get(url)
+    response.raise_for_status()  # Ensure we notice bad responses
+    return response.content
+
+
+def load_default_file():
+    """Load default file from Google Sheets and return DataFrame."""
+    file_id = '19VBAxnZP0cSIeHNNpv1InRzZDBddPM1M'  # Google Sheets dosyanızın ID'si
+    try:
+        file_content = download_file_from_google_sheets(file_id)
+
+        # Create a temporary file and write the downloaded content to it
+        with tempfile.NamedTemporaryFile(delete=False, suffix='.xlsx') as temp_file:
+            temp_file.write(file_content)
+            temp_file.flush()
+            temp_file_path = temp_file.name
+
+        # Read the file into a DataFrame
+        df = pd.read_excel(temp_file_path)
+        df = check_date_column(df)
+        if df is not None:
+            st.session_state['data'] = df
+            st.sidebar.info('Default data file loaded from Google Sheets.')
+        return df
+    except Exception as e:
+        st.sidebar.error(f'Error loading default file from Google Sheets: {e}')
+        return None
+
 
 def upload_file():
     """Handle file upload and return DataFrame."""
@@ -36,16 +72,3 @@ def upload_file():
     else:
         return load_default_file()
 
-def load_default_file():
-    """Load default file and return DataFrame."""
-    default_file_path = "/Users/asmir/Desktop/MyProjects/EnergyDemandPrediction/Book2.xlsx"
-    try:
-        df = pd.read_excel(default_file_path)
-        df = check_date_column(df)
-        if df is not None:
-            st.session_state['data'] = df
-            st.sidebar.info('Default data file loaded.')
-        return df
-    except Exception as e:
-        st.sidebar.error(f'Error loading default file: {e}')
-        return None
